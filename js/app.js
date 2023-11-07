3fb2f340e762d6acba265592ef1aa26ed24429e6
Hazel Windle <hwindle83@googlemail.com>
2023-11-07 14:24:51 +0000

    Did header area, and toggle JavaScript/animation

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// add style to move toggle right
+let toggle = document.querySelector('.toggle');
+let whiteSwitch = toggle.querySelector('span.white-switch');
+console.log(whiteSwitch);
+
+toggle.addEventListener('click', function() {
+  whiteSwitch.classList.toggle('move-right');
+});
